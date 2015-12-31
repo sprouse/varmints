82ec26517f5b653421f97b393402a87d39375b11
chris-torrence <chris_torrence@comcast.net>
2015-12-30 18:23:16 -0700

    First working version of master/slave

--- sf_8266_i2c/i2c_slave/i2c_slave.ino
+++ sf_8266_i2c/i2c_slave/i2c_slave.ino
@@ -8,12 +8,14 @@
 // Created 29 March 2006
 
 // This example code is in the public domain.
-
+#include <MicroView.h>
 #include <Wire.h>
 
+MicroViewWidget *widget, *widget2;
+
 union u_tag {
-   byte b[4];
-   float expAverageTemperatureF;
+  byte b[4];
+  float expAverageTemperatureF;
 } u;
 
 
@@ -21,9 +23,17 @@ void setup() {
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
   Serial.begin(115200);           // start serial for output
+
+  uView.begin();
+  uView.clear(PAGE);
+  widget = new MicroViewGauge(32, 30, 60, 80); // draw Gauge widget at x=32,y=30,min=0, max=100
+  widget2 = new MicroViewSlider(0, 0, 60, 80); // draw Slider widget at x=0,y=0,min=0, max=100
 }
 
 void loop() {
+  widget->setValue(u.expAverageTemperatureF);    // give a value to widget
+  widget2->setValue(u.expAverageTemperatureF);
+  uView.display();        // display current page buffer
   delay(100);
 }
 
@@ -32,13 +42,17 @@ void loop() {
 void receiveEvent(int howMany) {
 
   // Frame the data. Always start with a 0xaa;
-  while (Wire.available() && (Wire.read() != 0xaa));  // Burp non 0xaa characers;
+  byte c;
+  while (Wire.available() && (c = Wire.read() != 0xaa)) {
+    Serial.print("discard ");  // Burp non 0xaa characers;
+    Serial.println(c, HEX);
+  }
 
   // Make sure there are at least 4 characters in the buffer
   if (Wire.available() < 4) {
     Serial.println("Waiting for 4");
     return;
-  }  
+  }
   for (int i = 0; i < 4; i++) { // loop through all but the last
     u.b[i] = Wire.read(); // receive byte as a character
     Serial.print(u.b[i]);         // print the character
@@ -46,5 +60,7 @@ void receiveEvent(int howMany) {
   }   // receive byte as an integer
   Serial.println();
   Serial.println(u.expAverageTemperatureF);         // print the integer
+
+  Wire.flush();
 }
 
