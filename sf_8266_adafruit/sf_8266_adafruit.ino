259b0c54106054e78d71d2d3e3a4b5726299299d
Steven Sprouse <ssprouse@sandisk.com>
2016-02-01 13:29:16 -0800

    -- Initial sketch testing out new Adafruit IO service.

---
+++ sf_8266_adafruit/sf_8266_adafruit.ino
@@ -0,0 +1,114 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+// WiFi parameters
+#define WLAN_SSID       "CorpGuest"
+#define WLAN_PASS       ""
+
+// Adafruit IO
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "Gorp"
+#define AIO_KEY         "6afb3b610d14e11d077873ddb86e56f4dd2ff6f0"
+
+// Functions
+void connect();
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+
+// Store the MQTT server, client ID, username, and password in flash memory.
+const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
+
+// Set a unique MQTT client ID using the AIO key + the date and time the sketch
+// was compiled (so this should be unique across multiple devices for a user,
+// alternatively you can manually set this to a GUID or other random value).
+const char MQTT_CLIENTID[] PROGMEM  = AIO_KEY __DATE__ __TIME__;
+const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
+const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, AIO_SERVERPORT, MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);/****************************** Feeds ***************************************/
+
+// Setup feeds for temperature & humidity
+const char TEMPERATURE_FEED[] PROGMEM = AIO_USERNAME "/feeds/temperature";
+Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, TEMPERATURE_FEED);
+
+
+void setup() {
+    Serial.begin(115200);
+  Serial.println(F("Adafruit IO Example"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  delay(10);
+  Serial.print(F("Connecting to "));
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  Serial.println();
+
+  Serial.println(F("WiFi connected"));
+  Serial.println(F("IP address: "));
+  Serial.println(WiFi.localIP());
+
+  // connect to adafruit io
+  connect();
+}
+
+
+int i = 0;
+void loop() {
+  if (! mqtt.ping(3)) {
+    // reconnect to adafruit io
+    if (! mqtt.connected())
+      connect();
+  }
+
+
+  // Publish data
+  if (! temperature.publish(i))
+    Serial.println(F("Failed to publish temperature"));
+  else
+    Serial.println(F("Temperature published!"));
+
+  delay(5000);
+  i++;
+}
+
+
+// connect to adafruit io via MQTT
+void connect() {
+
+  Serial.print(F("Connecting to Adafruit IO... "));
+
+  int8_t ret;
+
+  while ((ret = mqtt.connect()) != 0) {
+
+    switch (ret) {
+      case 1: Serial.println(F("Wrong protocol")); break;
+      case 2: Serial.println(F("ID rejected")); break;
+      case 3: Serial.println(F("Server unavail")); break;
+      case 4: Serial.println(F("Bad user/pass")); break;
+      case 5: Serial.println(F("Not authed")); break;
+      case 6: Serial.println(F("Failed to subscribe")); break;
+      default: Serial.println(F("Connection failed")); break;
+    }
+
+    if(ret >= 0)
+      mqtt.disconnect();
+
+    Serial.println(F("Retrying connection..."));
+    delay(5000);
+
+  }
+
+  Serial.println(F("Adafruit IO Connected!"));
+
+}
